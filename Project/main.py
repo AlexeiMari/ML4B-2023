214f7b8e6f9ad5b28666e0a8517e252cb9bf3ba8
BelmontR <47500226+BelmontR@users.noreply.github.com>
2023-04-24 15:10:34 +0200

    Removed Data, opend Project

---
+++ Project/main.py
@@ -0,0 +1,10 @@
+#Hier werden schon Mal die wichtigsten Libs, die wir verwenden werden, importiert.
+#Geht sicher, dass ihr die installiert habt
+import pandas as pd
+import streamlit as st
+import csv
+import sklearn
+import numpy as np
+import json
+
+
