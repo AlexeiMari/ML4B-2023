6205bc7ce2d7d2defcf011d19afe9494c1f5f0f1
BelmontR <47500226+BelmontR@users.noreply.github.com>
2023-06-06 18:57:01 +0200

    Did Stuff, doesm't work. Hate it

--- Project/main.py
+++ Project/main.py
@@ -54,7 +54,16 @@ def process_data(upload):
         data = transform_data_json(r"C:\Users\ReneJ\Desktop\UnityStuff\ML4B-2023\Project\json.json")
 
     st.write(data)
-    return data
+    splitData = split_data(data, 1)
+    metrics = data_to_metric(splitData)
+    end = combine(metrics)
+
+    prediction = rnf.predict(end)
+
+    timeLineData = create_time_line_data(prediction)
+
+    st.write(timeLineData)
+    return timeLineData
 
 def transform_data_csv(file):
     datasets = {}  # Ein Dictionary
@@ -112,6 +121,116 @@ def transform_data_json(file):
         datasets[sensor] = temp
 
     return datasets
+
+def split_data(list, length_of_time_series):
+    splitted_list = []
+    for dict in list:
+        amount_of_splits = 999999999999
+        print(dict["Accelerometer"].iloc[-1]["ID"])
+        # print(dict)
+        for sensor in sensors:
+            temp_aos = math.floor(dict[sensor]["seconds_elapsed"].iloc[-1] / (60 * length_of_time_series))
+            if temp_aos < amount_of_splits:
+                amount_of_splits = temp_aos
+        print(amount_of_splits)
+        if amount_of_splits == 999999999999 or amount_of_splits <= 1:  # case 1: Something went wrong, we don't split. Case 2: The Timeseries is not long enough to be splited
+            # Wenn der Datensatz zu kurz zum splitten ist, wird er nicht gesplittet, stattdessen wird er einfach als ganzes in die splitted_list gelegt
+            splitted_list.append(dict)
+
+        else:
+            split_dict = {}  # Dieses dictionary wird jedem Sensor eine Liste von aufgesplitteten DFs zuweisen
+            split_dict["activity"] = dict["activity"]
+            for sensor in sensors:
+                splitted_dict_entry = np.array_split(dict[sensor],
+                                                     amount_of_splits)  # Das ist jetzt ne Liste mit aufgeteilten Dataframes
+                print(len(splitted_dict_entry))
+                id_suffix = 0
+                for df in splitted_dict_entry:
+                    df["ID"] = df["ID"] + "_" + str(id_suffix)
+                    id_suffix += 1
+
+                split_dict[sensor] = splitted_dict_entry
+
+            for i in range(0, amount_of_splits):
+                sub_dict = {}
+                sub_dict["activity"] = dict["activity"]
+                for sensor in sensors:
+                    sub_dict[sensor] = split_dict[sensor][i]
+                splitted_list.append(sub_dict)
+
+    return splitted_list
+
+def data_to_metric(list):
+    i = 0
+    final_form_data_list = []
+    for dict in list:
+        for sensor in sensors:
+            dict[sensor] = dict[sensor].drop(columns=["seconds_elapsed"])
+
+            temp = tsfresh.extract_features(dict[sensor], column_id = "ID",
+                                        default_fc_parameters=tsfresh.feature_extraction.MinimalFCParameters())
+            temp["activity"] = dict["activity"]
+            #temp["ID"] = dict[sensor]["ID"]
+            final_form_data_list.append({"data" : temp, "sensor" : sensor})
+            i += 1
+            print(i)
+    return final_form_data_list
+
+def combine(final_form_data_list):
+    very_final_form_data_list = []
+
+    for sensor in sensors:
+        temp_list = []
+        for dict in final_form_data_list:
+            if str(dict["sensor"]) == str(sensor):
+                temp_list.append(dict["data"])
+        concat_temp = pd.concat(temp_list)
+        very_final_form_data_list.append(concat_temp)
+
+    # Join all the Dataframes to one Dataframe
+    df_final = pd.concat(very_final_form_data_list, axis=1)
+
+    # Drop duplicate "activity" Columns
+    d = df_final.T.drop_duplicates().T
+    df_final = df_final.drop(columns=["activity"])
+    df_final["activity"] = d["activity"]
+
+    # Final Dataframe with all the transformed data
+    return df_final
+
+
+class activityCountMapper:
+    activity: str
+    count: int
+
+    def __init__(self, act: str):  # Konstruktor der Klasse
+        self.activity = act
+        self.count = 1
+
+    def countUp(self):
+        self.count += 1
+
+    def getActivity(self) -> str:
+        return self.activity
+
+    def getCount(self) -> int:
+        return self.count
+
+def create_time_line_data(dataList:list):
+    returnList = []
+    global latestElement
+    latestElement = None
+
+    for entry in dataList:
+        if latestElement == None:
+            latestElement = str(entry)
+            returnList.append(activityCountMapper(str(entry)))
+        elif str(entry) == latestElement:
+            returnList[len(returnList) -1].countUp()
+        elif str(entry) != latestElement:
+            returnList.append(activityCountMapper(str(entry)))
+
+    return returnList
 
 ### Pythonic Area
 
