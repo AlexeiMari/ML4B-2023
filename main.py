4450bdca5b8f0ad7c5ae41a871db1ee257c11b2c
AlexeiMari <alexei.marinov@fau.de>
2023-06-27 21:24:09 +0200

    #

--- main.py
+++ main.py
@@ -533,12 +533,12 @@ def main():
             paper_bgcolor='#282C34',
             font=dict(color='white')
         )
-        bar_chart, pie_chart = st.columns(2)
+        bar_chart, rechnungen, pie_chart = st.columns(3)
         bar_chart.plotly_chart(bar2)
-        pie_chart.plotly_chart(fig_pie)
-        if emission_roller + emission_auto > 1000:
-            emission_kg = round((emission_roller + emission_auto) / 1000, 2)
-            st.markdown(
+        with rechnungen:
+            if emission_roller + emission_auto > 1000:
+                emission_kg = round((emission_roller + emission_auto) / 1000, 2)
+                st.markdown(
                 f'<div style="background-color: #282C34; display: flex; flex-direction: column; align-items: center; justify-content: center; padding: 10px; border-radius: 5px; height: 150px; width:150px;">'
                 f'<div style="color: white; font-weight: bold; text-align: center;">Emissionen</div>'
                 f'<div style="color: white; font-size: 24px; text-align: center;">{emission_kg} kg CO2</div>'
@@ -546,9 +546,9 @@ def main():
                 '</div>',
                 unsafe_allow_html=True
                 )
-        else:
-            emission_kg1 = round((emission_roller + emission_auto) / 1000, 2)
-            st.markdown(
+            else:
+                emission_kg1 = round((emission_roller + emission_auto) / 1000, 2)
+                st.markdown(
                 f'<div style="background-color: #282C34; display: flex; flex-direction: column; align-items: center; justify-content: center; padding: 10px; border-radius: 5px; height: 150px; width:150px;">'
                 f'<div style="color: white; font-weight: bold; text-align: center;">Emissionen</div>'
                 f'<div style="color: white; font-size: 24px; text-align: center;">{emission_roller + emission_auto} g CO2</div>'
@@ -558,13 +558,15 @@ def main():
                 unsafe_allow_html=True
                 )
             #Quelle: https://www.umweltnetz-schweiz.ch/themen/energie/4166-co2-vergleich-motorroller-und-e-roller.html
-        st.markdown(
+            st.markdown(
             f'<div style="background-color: #282C34; display: flex; flex-direction: column; align-items: center; justify-content: center; padding: 10px; border-radius: 5px; height: 150px; width:150px;">'
             f'<div style="color: white; font-weight: bold; text-align: center;">Kalorien</div>'
             f'<div style="color: white; font-size: 24px; text-align: center;">{verbrauchte_kalorien_bike + verbrauchte_kalorien_walk}</div>'
             '</div>',
             unsafe_allow_html=True
-        )      
+        )          
+        pie_chart.plotly_chart(fig_pie)
+
         
 
 
