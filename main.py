d8b7a337f59df3e71c5e2365909a3e151bae4fbf
AlexeiMari <alexei.marinov@fau.de>
2023-06-27 22:15:39 +0200

    ##

--- main.py
+++ main.py
@@ -545,35 +545,36 @@ def main():
             st.plotly_chart(bar2)
         with pie_chart:
             st.plotly_chart(fig_pie)
-        if emission_roller + emission_auto > 1000:
-                emission_kg = round((emission_roller + emission_auto) / 1000, 2)
-                st.markdown(
+        with st.container():
+            if emission_roller + emission_auto > 1000:
+                    emission_kg = round((emission_roller + emission_auto) / 1000, 2)
+                    st.markdown(
+                    f'<div style="background-color: #282C34; display: flex; flex-direction: column; align-items: center; justify-content: center; padding: 10px; border-radius: 5px; height: 150px; width:150px;">'
+                    f'<div style="color: white; font-weight: bold; text-align: center;">Emissionen</div>'
+                    f'<div style="color: white; font-size: 24px; text-align: center;">{emission_kg} kg CO2</div>'
+                    f'<div style="color: white; font-size: 14px; text-align: center;">{emission_kg} kg / 33 kg CO2</div>'
+                    '</div>',
+                    unsafe_allow_html=True
+                    )
+            else:
+                    emission_kg1 = round((emission_roller + emission_auto) / 1000, 2)
+                    st.markdown(
+                    f'<div style="background-color: #282C34; display: flex; flex-direction: column; align-items: center; justify-content: center; padding: 10px; border-radius: 5px; height: 150px; width:150px;">'
+                    f'<div style="color: white; font-weight: bold; text-align: center;">Emissionen</div>'
+                    f'<div style="color: white; font-size: 24px; text-align: center;">{emission_roller + emission_auto} g CO2</div>'
+                    f'<div style="color: white; font-size: 14px; text-align: center;">{emission_kg1} kg / 33 kg CO2</div>'
+                    #Quelle: https://de.statista.com/statistik/daten/studie/1275275/umfrage/treibhausgasbilanz-pro-person/
+                    '</div>',
+                    unsafe_allow_html=True
+                    )
+                #Quelle: https://www.umweltnetz-schweiz.ch/themen/energie/4166-co2-vergleich-motorroller-und-e-roller.html
+            st.markdown(
                 f'<div style="background-color: #282C34; display: flex; flex-direction: column; align-items: center; justify-content: center; padding: 10px; border-radius: 5px; height: 150px; width:150px;">'
-                f'<div style="color: white; font-weight: bold; text-align: center;">Emissionen</div>'
-                f'<div style="color: white; font-size: 24px; text-align: center;">{emission_kg} kg CO2</div>'
-                f'<div style="color: white; font-size: 14px; text-align: center;">{emission_kg} kg / 33 kg CO2</div>'
+                f'<div style="color: white; font-weight: bold; text-align: center;">Kalorien</div>'
+                f'<div style="color: white; font-size: 24px; text-align: center;">{verbrauchte_kalorien_bike + verbrauchte_kalorien_walk}</div>'
                 '</div>',
                 unsafe_allow_html=True
-                )
-        else:
-                emission_kg1 = round((emission_roller + emission_auto) / 1000, 2)
-                st.markdown(
-                f'<div style="background-color: #282C34; display: flex; flex-direction: column; align-items: center; justify-content: center; padding: 10px; border-radius: 5px; height: 150px; width:150px;">'
-                f'<div style="color: white; font-weight: bold; text-align: center;">Emissionen</div>'
-                f'<div style="color: white; font-size: 24px; text-align: center;">{emission_roller + emission_auto} g CO2</div>'
-                f'<div style="color: white; font-size: 14px; text-align: center;">{emission_kg1} kg / 33 kg CO2</div>'
-                #Quelle: https://de.statista.com/statistik/daten/studie/1275275/umfrage/treibhausgasbilanz-pro-person/
-                '</div>',
-                unsafe_allow_html=True
-                )
-            #Quelle: https://www.umweltnetz-schweiz.ch/themen/energie/4166-co2-vergleich-motorroller-und-e-roller.html
-        st.markdown(
-            f'<div style="background-color: #282C34; display: flex; flex-direction: column; align-items: center; justify-content: center; padding: 10px; border-radius: 5px; height: 150px; width:150px;">'
-            f'<div style="color: white; font-weight: bold; text-align: center;">Kalorien</div>'
-            f'<div style="color: white; font-size: 24px; text-align: center;">{verbrauchte_kalorien_bike + verbrauchte_kalorien_walk}</div>'
-            '</div>',
-            unsafe_allow_html=True
-        )         
+            )         
 
         
 
