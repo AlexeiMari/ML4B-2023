5aaa749a6447c0b21a44566bca57db328ebe4a33
AlexeiMari <alexei.marinov@fau.de>
2023-06-27 13:42:40 +0200

    ##

--- main.py
+++ main.py
@@ -349,7 +349,7 @@ def main():
     uploaded_file = st.file_uploader("Bitte lade Sensordaten hoch. Diese können ein .JSON-Format oder auch ein .zip-Format haben, die CSVs enthalten.", accept_multiple_files=False)
     if st.button("Klassifizieren!"):
         prediction_data, gps, metric_data, raw_predictions, start_minutes = process_data(uploaded_file)
-        st.markdown('<a href="/dashboard?page=dashboard" target="_self"><button>Previous</button></a>', unsafe_allow_html=True)
+        st.markdown('<a href="/dashboard?page=dashboard" target="_self" style="background-color: #F63366; color: white; padding: 8px 16px; border: none; text-decoration: none; font-weight: bold; border-radius: 4px; cursor: pointer;">Dashboard</a>', unsafe_allow_html=True)
         #SUPER WICHTIG!!! BITTE LESEN
         #
         # prediction_data = geordnete Tupelliste. Jedes Tupel speichert eine Aktivität, eine Länge in Minuten und die Stunde, zu der die Aktivität gestartet wurde.
