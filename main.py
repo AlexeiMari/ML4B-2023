f47b96111c9e2550b345e7dcda7b003808db1aa6
AlexeiMari <alexei.marinov@fau.de>
2023-06-27 22:57:10 +0200

    ##

--- main.py
+++ main.py
@@ -542,6 +542,7 @@ def main():
 
         bar_chart, pie_chart = st.columns(2)
         with bar_chart:
+            st.plotly_chart(bar2)
             with st.container():
                 if emission_roller + emission_auto > 1000:
                         emission_kg = round((emission_roller + emission_auto) / 1000, 2)
