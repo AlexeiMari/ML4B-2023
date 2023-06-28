4825368a184fcd5ee9fffe1571e4baa51ef36afa
BelmontR <47500226+BelmontR@users.noreply.github.com>
2023-06-28 12:35:06 +0200

    Fix?

--- main.py
+++ main.py
@@ -619,7 +619,7 @@ def main():
         bar_data = []
         for category, subdict in hour_act_dict.items():
             bar_data.append(
-                go.Bar(name=category, color = aktivitaeten_farben,x=subcategories, y=[subdict.get(subcat, 0) for subcat in subcategories]))
+                go.Bar(name=category, color = aktivitaeten_farben[category] ,x=subcategories, y=[subdict.get(subcat, 0) for subcat in subcategories]))
 
         # Layout für das Diagramm definieren
         layout = go.Layout(barmode='group', xaxis={'title': 'Stunde des Tages'}, yaxis={'title': 'Minuten'})
