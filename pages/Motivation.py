4fa1baed748a354c72f95ff7a3b317650a96d937
BelmontR <47500226+BelmontR@users.noreply.github.com>
2023-07-04 16:42:55 +0200

    Map

--- pages/Motivation.py
+++ pages/Motivation.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import pandas as pd
 import streamlit as st 
 from streamlit_lottie import st_lottie
 
@@ -26,6 +27,8 @@ with student:
 st.subheader("Deswegen kam uns eine geniale Idee!")
 st.write("Du kannst tatsächlich deinen Weg von zuhause bis zur Uni verfolgen, indem du die Sensordaten deines Smartphones nutzt. Das Ding zeichnet quasi auf, wie du dich fortbewegst - ob du läufst, radelst oder dich wie ein gestrandeter Wal auf öffentliche Verkehrsmittel wirfst. Du kannst dann genau sehen, welches Verkehrsmittel du genommen hast und wie viel Zeit du dafür brauchst. Echt praktisch, um herauszufinden, ob du besser den Bus nimmst oder doch evtl. das Auto.")
 st.header("Hier kommt die Karte hin")
+map_df = pd.read_csv("LocationConcat.csv")
+st.map(map_df)
 
 st.subheader("Was ist noch möglich?")
 st.write("Aber das ist noch längst nicht alles! Hier kommt der Clou: Stell dir vor, du bist ein echter Berufspendler und zeichnest deine tägliche Strecke auf. Dann kannst du zu deinem Boss marschieren und ihm die Daten präsentieren. Wer weiß, vielleicht handelst du dir ein schickes Geschäftsauto ein oder verlangst, dass deine Kosten für öffentliche Verkehrsmittel vom Unternehmen übernommen werden. Das ist doch mal eine geniale Möglichkeit, deine Pendelei in bare Münze umzuwandeln!")
